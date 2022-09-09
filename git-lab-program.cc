a6df540182fe3f4751aa0f3de40ff02e5105e57b
TyLinkous <tl987021@ohio.edu>
2022-09-09 15:41:56 -0400

    made a code thing.

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ty Linkous
+ *        Date: 9/9/2022
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
